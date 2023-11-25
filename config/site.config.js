0de463c5712d95a3d83901f04ab217cdf38ec4ce
刘彤 <94297156+JiemoGui@users.noreply.github.com>
2023-11-25 15:40:07 +0800

    Update site.config.js
    修改了网站标题

--- config/site.config.js
+++ config/site.config.js
@@ -17,7 +17,7 @@ module.exports = {
   kvPrefix: process.env.KV_PREFIX || '',
 
   // The name of your website. Present alongside your icon.
-  title: "Spencer's OneDrive",
+  title: "芥末鬼の分享",
 
   // The folder that you are to share publicly with onedrive-vercel-index. Use '/' if you want to share your root folder.
   baseDirectory: '/Public',
